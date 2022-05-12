4293bec6978705f3d740b0a64bd8843d60d8cf6c
damarisdwayne <damarisdwayne@outlook.com>
2022-05-12 11:14:42 -0300

    integrando aplicação com react hook form para gerenciamento de estado dos inputs

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
+import {
+    Modal,
+    TouchableWithoutFeedback,
+    Keyboard,
+    Alert
+} from 'react-native'
+import * as Yup from 'yup'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { Input } from '../../components/Forms/Input';
+import { InputForm } from '../../components/Forms/InputForm'
 import { Button } from '../../components/Forms/Button';
+import { CategorySelectButton } from '../../components/Forms/CategorySelectButton';
 
 import {
     Container,
@@ -13,48 +24,126 @@ import {
 
 } from './styles';
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
+import { CategorySelect } from '../CategorySelect';
+
+interface FormData {
+    name: string
+    amount: string
+}
 
 export function Register() {
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria'
+    })
+
     const [transactionType, setTransactionType] = useState('')
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false)
+
+    const schema = Yup.object({
+        name: Yup.string().required('Nome é obrigatório'),
+        amount: Yup.number()
+            .typeError('Informe um valor numérico')
+            .positive('O valor não pode ser negativo')
+    })
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    })
 
     function handleTransactionTypeSelect(type: 'up' | 'down') {
         setTransactionType(type)
     }
+
+    function handleOpenSelectCategoryModal() {
+        setCategoryModalOpen(true)
+    }
+
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false)
+    }
+
+    function handleRegister(form: FormData) {
+        if (!transactionType)
+            return Alert.alert('Selecione o tipo da transação')
+
+        if (category.key === 'category')
+            return Alert.alert('Selecione a categoria')
+
+
+        const data = {
+            name: form.name,
+            amount: form.amount,
+            transactionType,
+            category: category.key
+        }
+        console.log(data)
+    }
+
     return (
-        <Container>
-            <Header>
-                <Title>Cadastro</Title>
-            </Header>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Container>
+                <Header>
+                    <Title>Cadastro</Title>
+                </Header>
 
-            <Form>
-                <Fields>
-                    <Input
-                        placeholder='Nome'
-                    />
-                    <Input
-                        placeholder='Email'
-                    />
-
-                    <TransactionsTypes>
-                        <TransactionTypeButton
-                            type='up'
-                            title='Income'
-                            onPress={() => handleTransactionTypeSelect('up')}
-                            isActive={transactionType === 'up'}
+                <Form>
+                    <Fields>
+                        <InputForm
+                            name='name'
+                            control={control}
+                            placeholder="nome"
+                            autoCapitalize='sentences'
+                            autoCorrect={false}
+                            error={errors.name && errors.name.message}
                         />
-                        <TransactionTypeButton
-                            type='down'
-                            title='Income'
-                            onPress={() => handleTransactionTypeSelect('down')}
-                            isActive={transactionType === 'down'}
+                        <InputForm
+                            name='amount'
+                            control={control}
+                            placeholder='Preço'
+                            keyboardType='numeric'
+                            error={errors.amount && errors.amount.message}
                         />
-                    </TransactionsTypes>
-                </Fields>
 
-                <Button
-                    title='Enviar'
-                />
-            </Form>
-        </Container>
+                        <TransactionsTypes>
+                            <TransactionTypeButton
+                                type='up'
+                                title='Income'
+                                onPress={() => handleTransactionTypeSelect('up')}
+                                isActive={transactionType === 'up'}
+                            />
+                            <TransactionTypeButton
+                                type='down'
+                                title='Income'
+                                onPress={() => handleTransactionTypeSelect('down')}
+                                isActive={transactionType === 'down'}
+                            />
+                        </TransactionsTypes>
+
+                        <CategorySelectButton
+                            title={category.name}
+                            onPress={handleOpenSelectCategoryModal}
+                        />
+                    </Fields>
+
+                    <Button
+                        title='Enviar'
+                        onPress={handleSubmit(handleRegister)}
+                    />
+                </Form>
+
+                <Modal visible={categoryModalOpen}>
+                    <CategorySelect
+                        category={category}
+                        setCategory={setCategory}
+                        closeSelectCategory={handleCloseSelectCategoryModal}
+                    />
+                </Modal>
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
